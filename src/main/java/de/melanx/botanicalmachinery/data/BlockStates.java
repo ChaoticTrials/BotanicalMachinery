99429184c664a2a554b654788c09d045d314438d
MelanX <info@melanx.de>
2020-07-16 23:23:45 +0200

    changed texture

--- src/main/java/de/melanx/botanicalmachinery/data/BlockStates.java
+++ src/main/java/de/melanx/botanicalmachinery/data/BlockStates.java
@@ -5,9 +5,7 @@ import de.melanx.botanicalmachinery.core.Registration;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.*;
-import vazkii.botania.common.lib.LibMisc;
 
 public class BlockStates extends BlockStateProvider {
     public BlockStates(DataGenerator gen, ExistingFileHelper helper) {
@@ -18,7 +16,8 @@ public class BlockStates extends BlockStateProvider {
     protected void registerStatesAndModels() {
         Registration.BLOCKS.getEntries().stream().map(block -> block.get()).forEach(block -> {
             VariantBlockStateBuilder builder = getVariantBuilder(block);
-            ModelFile model = models().orientable(block.getRegistryName().getPath(), new ResourceLocation(LibMisc.MOD_ID, "blocks/livingrock0"), modLoc("block/" + block.getRegistryName().getPath()), new ResourceLocation(LibMisc.MOD_ID, "blocks/livingrock0"));
+            String name = block.getRegistryName().getPath();
+            ModelFile model = models().orientable(block.getRegistryName().getPath(), modLoc("block/" + name + "_side"), modLoc("block/" + name + "_front"), modLoc("block/" + name + "_top"));
             for (Direction direction : BlockStateProperties.HORIZONTAL_FACING.getAllowedValues()) {
                 builder.partialState().with(BlockStateProperties.HORIZONTAL_FACING, direction)
                         .addModels(new ConfiguredModel(model, direction.getHorizontalIndex() == -1 ? direction.getOpposite().getAxisDirection().getOffset() * 90 : 0, (int) direction.getOpposite().getHorizontalAngle(), false));
