015b52e44f26504a16d7c1aae6ea18b7a1a31b67
MelanX <info@melanx.de>
2020-07-30 09:31:15 +0200

    add jei compact for altar

--- src/main/java/de/melanx/botanicalmachinery/compat/BotanicalMachineryPlugin.java
+++ src/main/java/de/melanx/botanicalmachinery/compat/BotanicalMachineryPlugin.java
@@ -2,6 +2,7 @@ package de.melanx.botanicalmachinery.compat;
 
 import de.melanx.botanicalmachinery.BotanicalMachinery;
 import de.melanx.botanicalmachinery.blocks.screens.ScreenMechanicalManaPool;
+import de.melanx.botanicalmachinery.blocks.screens.ScreenMechanicalRunicAltar;
 import de.melanx.botanicalmachinery.core.Registration;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
@@ -10,6 +11,7 @@ import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import vazkii.botania.client.integration.jei.manapool.ManaPoolRecipeCategory;
+import vazkii.botania.client.integration.jei.runicaltar.RunicAltarRecipeCategory;
 
 @JeiPlugin
 public class BotanicalMachineryPlugin implements IModPlugin {
@@ -21,10 +23,12 @@ public class BotanicalMachineryPlugin implements IModPlugin {
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
         registration.addRecipeClickArea(ScreenMechanicalManaPool.class, 77, 36, 22, 15, ManaPoolRecipeCategory.UID);
+        registration.addRecipeClickArea(ScreenMechanicalRunicAltar.class, 87, 65, 22, 15, RunicAltarRecipeCategory.UID);
     }
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
         registration.addRecipeCatalyst(new ItemStack(Registration.BLOCK_MECHANICAL_MANA_POOL.get()), ManaPoolRecipeCategory.UID);
+        registration.addRecipeCatalyst(new ItemStack(Registration.BLOCK_MECHANICAL_RUNIC_ALTAR.get()), RunicAltarRecipeCategory.UID);
     }
 }
