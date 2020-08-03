fa85f0e737eb21859faf54242da8f0532c78e50e
MelanX <info@melanx.de>
2020-08-04 00:25:51 +0200

    added alfheim market to jei stuff

--- src/main/java/de/melanx/botanicalmachinery/compat/BotanicalMachineryPlugin.java
+++ src/main/java/de/melanx/botanicalmachinery/compat/BotanicalMachineryPlugin.java
@@ -1,6 +1,7 @@
 package de.melanx.botanicalmachinery.compat;
 
 import de.melanx.botanicalmachinery.BotanicalMachinery;
+import de.melanx.botanicalmachinery.blocks.screens.ScreenAlfheimMarket;
 import de.melanx.botanicalmachinery.blocks.screens.ScreenMechanicalManaPool;
 import de.melanx.botanicalmachinery.blocks.screens.ScreenMechanicalRunicAltar;
 import de.melanx.botanicalmachinery.core.Registration;
@@ -10,6 +11,7 @@ import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import vazkii.botania.client.integration.jei.elventrade.ElvenTradeRecipeCategory;
 import vazkii.botania.client.integration.jei.manapool.ManaPoolRecipeCategory;
 import vazkii.botania.client.integration.jei.runicaltar.RunicAltarRecipeCategory;
 
@@ -22,12 +24,14 @@ public class BotanicalMachineryPlugin implements IModPlugin {
 
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(ScreenAlfheimMarket.class, 77, 36, 22, 15, ElvenTradeRecipeCategory.UID);
         registration.addRecipeClickArea(ScreenMechanicalManaPool.class, 77, 36, 22, 15, ManaPoolRecipeCategory.UID);
         registration.addRecipeClickArea(ScreenMechanicalRunicAltar.class, 87, 65, 22, 15, RunicAltarRecipeCategory.UID);
     }
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(Registration.BLOCK_ALFHEIM_MARKET.get()), ElvenTradeRecipeCategory.UID);
         registration.addRecipeCatalyst(new ItemStack(Registration.BLOCK_MECHANICAL_MANA_POOL.get()), ManaPoolRecipeCategory.UID);
         registration.addRecipeCatalyst(new ItemStack(Registration.BLOCK_MECHANICAL_RUNIC_ALTAR.get()), RunicAltarRecipeCategory.UID);
     }
