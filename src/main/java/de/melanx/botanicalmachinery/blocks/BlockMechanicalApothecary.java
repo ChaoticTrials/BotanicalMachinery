2d6da4713bc8b6a3bfb375f8714e9629d4a4fcc9
MelanX <info@melanx.de>
2020-08-20 18:59:35 +0200

    right click with bucket to fill

--- src/main/java/de/melanx/botanicalmachinery/blocks/BlockMechanicalApothecary.java
+++ src/main/java/de/melanx/botanicalmachinery/blocks/BlockMechanicalApothecary.java
@@ -2,9 +2,7 @@ package de.melanx.botanicalmachinery.blocks;
 
 import de.melanx.botanicalmachinery.BotanicalMachinery;
 import de.melanx.botanicalmachinery.blocks.containers.ContainerMechanicalApothecary;
-import de.melanx.botanicalmachinery.blocks.containers.ContainerMechanicalDaisy;
 import de.melanx.botanicalmachinery.blocks.tiles.TileMechanicalApothecary;
-import de.melanx.botanicalmachinery.blocks.tiles.TileMechanicalDaisy;
 import de.melanx.botanicalmachinery.core.LibNames;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -14,6 +12,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -23,6 +22,9 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidActionResult;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nonnull;
@@ -50,6 +52,17 @@ public class BlockMechanicalApothecary extends Block {
     public ActionResultType onBlockActivated(@Nonnull BlockState state, World world, @Nonnull BlockPos pos, @Nonnull PlayerEntity player, @Nonnull Hand hand, @Nonnull BlockRayTraceResult hit) {
         if (!world.isRemote) {
             TileEntity tile = world.getTileEntity(pos);
+
+            ItemStack held = player.getHeldItemMainhand();
+            FluidActionResult fluidActionResult = FluidUtil.tryEmptyContainer(held, tile.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null).orElse(null), 1000, player, true);
+            if (fluidActionResult.isSuccess()) {
+                if (!player.isCreative()) {
+                    player.addItemStackToInventory(fluidActionResult.getResult());
+                    held.shrink(1);
+                }
+                return ActionResultType.SUCCESS;
+            }
+
             if (tile instanceof TileMechanicalApothecary) {
                 INamedContainerProvider containerProvider = new INamedContainerProvider() {
                     @Override
