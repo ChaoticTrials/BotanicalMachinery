4ca636f282364c8bbefc21adedae18de65c28d10
MelanX <info@melanx.de>
2020-08-20 19:40:56 +0200

    added fluid bar

--- src/main/java/de/melanx/botanicalmachinery/blocks/screens/ScreenMechanicalApothecary.java
+++ src/main/java/de/melanx/botanicalmachinery/blocks/screens/ScreenMechanicalApothecary.java
@@ -4,22 +4,22 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import de.melanx.botanicalmachinery.blocks.containers.ContainerMechanicalApothecary;
 import de.melanx.botanicalmachinery.blocks.tiles.TileMechanicalApothecary;
 import de.melanx.botanicalmachinery.core.LibResources;
-import net.minecraft.block.IBucketPickupHandler;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.container.PlayerContainer;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
 import java.awt.*;
 
 public class ScreenMechanicalApothecary extends ContainerScreen<ContainerMechanicalApothecary> {
+    private final int relX;
+    private final int relY;
+
     public ScreenMechanicalApothecary(ContainerMechanicalApothecary screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
-        this.xSize = 195;
-        this.ySize = 196;
+        this.xSize = 196;
+        this.ySize = 195;
+        this.relX = (this.width - this.xSize) / 2;
+        this.relY = (this.height - this.ySize) / 2;
     }
 
     @Override
@@ -29,9 +29,8 @@ public class ScreenMechanicalApothecary extends ContainerScreen<ContainerMechani
         GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         //noinspection ConstantConditions
         this.minecraft.getTextureManager().bindTexture(LibResources.MECHANICAL_APOTHECARY_GUI);
-        int relX = (this.width - this.xSize) / 2;
-        int relY = (this.height - this.ySize) / 2;
-        this.blit(relX, relY, 0, 0, this.xSize, this.ySize);
+
+        this.blit(this.relX, this.relY, 0, 0, this.xSize, this.ySize);
     }
 
     @Override
@@ -40,5 +39,9 @@ public class ScreenMechanicalApothecary extends ContainerScreen<ContainerMechani
         this.font.drawString(s, (float) (this.xSize / 2 - this.font.getStringWidth(s) / 2), 6.0F, Color.DARK_GRAY.getRGB());
         this.font.drawString(this.playerInventory.getDisplayName().getFormattedText(), 8.0F, (float) (this.ySize - 96 + 2), Color.DARK_GRAY.getRGB());
         this.font.drawString(String.valueOf(((TileMechanicalApothecary) this.container.getWorld().getTileEntity(this.container.getPos())).getFluidInventory().getFluidAmount()), 160.0F, this.ySize - 94, Color.BLUE.getRGB());
+
+        //noinspection ConstantConditions
+        this.minecraft.getTextureManager().bindTexture(LibResources.MECHANICAL_APOTHECARY_GUI);
+        this.blit(this.relX + 163, this.relY + 15, this.xSize, 0, 17, 81);
     }
 }
