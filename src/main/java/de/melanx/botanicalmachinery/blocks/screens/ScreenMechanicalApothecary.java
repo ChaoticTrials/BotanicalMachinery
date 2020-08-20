01ecde9ea03dbc7361e874544e265f60d47e0a18
MelanX <info@melanx.de>
2020-08-20 18:02:18 +0200

    basically added inventories

--- src/main/java/de/melanx/botanicalmachinery/blocks/screens/ScreenMechanicalApothecary.java
+++ src/main/java/de/melanx/botanicalmachinery/blocks/screens/ScreenMechanicalApothecary.java
@@ -2,14 +2,24 @@ package de.melanx.botanicalmachinery.blocks.screens;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import de.melanx.botanicalmachinery.blocks.containers.ContainerMechanicalApothecary;
+import de.melanx.botanicalmachinery.blocks.tiles.TileMechanicalApothecary;
 import de.melanx.botanicalmachinery.core.LibResources;
+import net.minecraft.block.IBucketPickupHandler;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.PlayerContainer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+
+import java.awt.*;
 
 public class ScreenMechanicalApothecary extends ContainerScreen<ContainerMechanicalApothecary> {
     public ScreenMechanicalApothecary(ContainerMechanicalApothecary screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
+        this.xSize = 195;
+        this.ySize = 196;
     }
 
     @Override
@@ -22,5 +32,13 @@ public class ScreenMechanicalApothecary extends ContainerScreen<ContainerMechani
         int relX = (this.width - this.xSize) / 2;
         int relY = (this.height - this.ySize) / 2;
         this.blit(relX, relY, 0, 0, this.xSize, this.ySize);
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        String s = this.title.getFormattedText();
+        this.font.drawString(s, (float) (this.xSize / 2 - this.font.getStringWidth(s) / 2), 6.0F, Color.DARK_GRAY.getRGB());
+        this.font.drawString(this.playerInventory.getDisplayName().getFormattedText(), 8.0F, (float) (this.ySize - 96 + 2), Color.DARK_GRAY.getRGB());
+        this.font.drawString(String.valueOf(((TileMechanicalApothecary) this.container.getWorld().getTileEntity(this.container.getPos())).getFluidInventory().getFluidAmount()), 160.0F, this.ySize - 94, Color.BLUE.getRGB());
     }
 }
--- src/main/java/de/melanx/botanicalmachinery/blocks/tiles/TileMechanicalApothecary.java
+++ src/main/java/de/melanx/botanicalmachinery/blocks/tiles/TileMechanicalApothecary.java
@@ -1,17 +1,108 @@
 package de.melanx.botanicalmachinery.blocks.tiles;
 
 import de.melanx.botanicalmachinery.core.Registration;
+import de.melanx.botanicalmachinery.util.inventory.BaseItemStackHandler;
+import de.melanx.botanicalmachinery.util.inventory.ItemStackHandlerWrapper;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import vazkii.botania.api.internal.VanillaPacketDispatcher;
+import vazkii.botania.api.recipe.IPetalRecipe;
 import vazkii.botania.common.block.tile.TileMod;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.function.Predicate;
+
 public class TileMechanicalApothecary extends TileMod implements ITickableTileEntity {
+
+    private static final int WORKING_DURATION = 20;
+    public static final String TAG_INV = "inv";
+    public static final String TAG_FLUID = "fluid";
+    public static final String TAG_PROGRESS = "progress";
+
+    private final LazyOptional<IItemHandlerModifiable> handler = ItemStackHandlerWrapper.createLazy(this::getInventory);
+    private final BaseItemStackHandler inventory = new BaseItemStackHandler(20, slot -> this.update = true, this::isValidStack);
+    private final ModdedFluidTank fluidInventory = new ModdedFluidTank(8000, fluidStack -> fluidStack.getFluid().isEquivalentTo(Fluids.WATER));
+    private final LazyOptional<IFluidHandler> fluidHandler = LazyOptional.of(() -> this.fluidInventory);
+    private IPetalRecipe recipe = null;
+    private boolean initDone;
+    private int progress;
+    private boolean update;
+    private boolean sendPacket;
 
     public TileMechanicalApothecary() {
         super(Registration.TILE_MECHANICAL_APOTHECARY.get());
     }
 
+    @Nonnull
+    public BaseItemStackHandler getInventory() {
+        return this.inventory;
+    }
+
+    @Nonnull
+    public FluidTank getFluidInventory() {
+        return this.fluidInventory;
+    }
+
+    public boolean isValidStack(int slot, ItemStack stack) {
+        return true;
+    }
+
     @Override
     public void tick() {
+        if (this.sendPacket) {
+            VanillaPacketDispatcher.dispatchTEToNearbyPlayers(this);
+            this.sendPacket = false;
+        }
+    }
 
+    @Override
+    public void writePacketNBT(CompoundNBT cmp) {
+        cmp.put(TAG_INV, this.getInventory().serializeNBT());
+        final CompoundNBT tankTag = new CompoundNBT();
+        this.getFluidInventory().getFluid().writeToNBT(tankTag);
+        cmp.put(TAG_FLUID, tankTag);
+        cmp.putInt(TAG_PROGRESS, this.progress);
+    }
+
+    @Override
+    public void readPacketNBT(CompoundNBT cmp) {
+        this.getInventory().deserializeNBT(cmp.getCompound(TAG_INV));
+        this.fluidInventory.setFluid(FluidStack.loadFluidStackFromNBT(cmp.getCompound(TAG_FLUID)));
+        this.progress = cmp.getInt(TAG_PROGRESS);
+    }
+
+    @Nonnull
+    @Override
+    public <X> LazyOptional<X> getCapability(@Nonnull Capability<X> cap, @Nullable Direction side) {
+        if (!this.removed && (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)) {
+            return this.handler.cast();
+        } else if (cap == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return this.fluidHandler.cast();
+        }
+        return super.getCapability(cap, side);
+    }
+
+    private class ModdedFluidTank extends FluidTank {
+        public ModdedFluidTank(int capacity, Predicate<FluidStack> validator) {
+            super(capacity, validator);
+        }
+
+        @Override
+        protected void onContentsChanged() {
+            TileMechanicalApothecary.this.sendPacket = true;
+            TileMechanicalApothecary.this.update = true;
+        }
     }
 }
