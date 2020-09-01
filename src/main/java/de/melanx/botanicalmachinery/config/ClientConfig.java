72f374b919777f524c09dc66a79b5d681bb499ed
MelanX <info@melanx.de>
2020-09-01 17:01:51 +0200

    added values for disabling tesr effects

--- src/main/java/de/melanx/botanicalmachinery/config/ClientConfig.java
+++ src/main/java/de/melanx/botanicalmachinery/config/ClientConfig.java
@@ -17,9 +17,31 @@ public class ClientConfig {
 
     public static ForgeConfigSpec.BooleanValue numericalMana;
 
+    public static ForgeConfigSpec.BooleanValue everything;
+    public static ForgeConfigSpec.BooleanValue alfheimMarket;
+    public static ForgeConfigSpec.BooleanValue agglomerationFactory;
+    public static ForgeConfigSpec.BooleanValue apothecary;
+    public static ForgeConfigSpec.BooleanValue brewery;
+    public static ForgeConfigSpec.BooleanValue daisy;
+    public static ForgeConfigSpec.BooleanValue manaPool;
+    public static ForgeConfigSpec.BooleanValue runicAltar;
+
     public static void init(ForgeConfigSpec.Builder builder) {
         numericalMana = builder.comment("Should mana in GUIs be displayed with numbers?")
                 .define("numericalMana", true);
+
+        builder.push("advanced-rendering");
+        builder.comment("Should the machine render its specific rendering if items are in the machine?");
+        everything = builder.comment("If you turn this off, the rendering is disabled for all machines and ignores the other config options")
+                .define("all", true);
+        alfheimMarket = builder.define("alfheim-market", true);
+        agglomerationFactory = builder.define("industrial-agglomeration-factory", true);
+        apothecary = builder.define("mechanical-apothecary", true);
+        brewery = builder.define("mechanical-brewery", true);
+        daisy = builder.define("mechanical-daisy", true);
+        manaPool = builder.define("mechanical-mana-pool", true);
+        runicAltar = builder.define("mechanical-runic-altar", true);
+        builder.pop();
     }
 
     public static void loadConfig(ForgeConfigSpec spec, Path path) {
