f638ab2704501979752f6c291dadccd25069a73a
MelanX <info@melanx.de>
2020-09-22 17:54:37 +0200

    add sound to clicking buttons
    close #32

--- src/main/java/de/melanx/botanicalmachinery/blocks/screens/ScreenManaBattery.java
+++ src/main/java/de/melanx/botanicalmachinery/blocks/screens/ScreenManaBattery.java
@@ -5,19 +5,17 @@ import de.melanx.botanicalmachinery.blocks.base.ScreenBase;
 import de.melanx.botanicalmachinery.blocks.containers.ContainerManaBattery;
 import de.melanx.botanicalmachinery.blocks.tiles.TileManaBattery;
 import de.melanx.botanicalmachinery.core.LibResources;
+import de.melanx.botanicalmachinery.helper.SoundHelper;
 import de.melanx.botanicalmachinery.network.BotanicalMachineryNetwork;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 
 import javax.annotation.Nonnull;
 
 public class ScreenManaBattery extends ScreenBase<ContainerManaBattery> {
-
-    boolean slot1Locked = false;
-    boolean slot2Locked = false;
-
     private int xB1;
     private int yB1;
     private int xB2;
@@ -53,7 +51,7 @@ public class ScreenManaBattery extends ScreenBase<ContainerManaBattery> {
         }
 
         if (mouseX >= this.xB2 && mouseX < this.xB2 + 20 && mouseY >= this.yB2 && mouseY < this.yB2 + 20) {
-            this.blit(ms, this.xB2, this.yB2, 20, tile.isSlot2Locked() ? this.ySize+ 20 : this.ySize, 20, 20);
+            this.blit(this.xB2, this.yB2, 20, tile.isSlot2Locked() ? this.ySize + 20 : this.ySize, 20, 20);
         } else {
             this.blit(ms, this.xB2, this.yB2, 0, tile.isSlot2Locked() ? this.ySize + 20 : this.ySize, 20, 20);
         }
@@ -68,10 +66,12 @@ public class ScreenManaBattery extends ScreenBase<ContainerManaBattery> {
             if (mouseX >= this.xB1 && mouseX < this.xB1 + 20 && mouseY >= this.yB1 && mouseY < this.yB1 + 20) {
                 tile.setSlot1Locked(!tile.isSlot1Locked());
                 BotanicalMachineryNetwork.updateLockedState(tile);
+                SoundHelper.playSound(SoundEvents.UI_BUTTON_CLICK);
             }
             if (mouseX >= this.xB2 && mouseX < this.xB2 + 20 && mouseY >= this.yB2 && mouseY < this.yB2 + 20) {
                 tile.setSlot2Locked(!tile.isSlot2Locked());
                 BotanicalMachineryNetwork.updateLockedState(tile);
+                SoundHelper.playSound(SoundEvents.UI_BUTTON_CLICK);
             }
         }
         return super.mouseClicked(mouseX, mouseY, clickedButton);
