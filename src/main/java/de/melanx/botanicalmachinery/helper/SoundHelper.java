65679e25f3f20bbe94b8cec604c8506ad83df94f
MelanX <info@melanx.de>
2020-09-22 16:23:40 +0200

    add sound to clicking buttons
    close #32

---
+++ src/main/java/de/melanx/botanicalmachinery/helper/SoundHelper.java
@@ -0,0 +1,16 @@
+package de.melanx.botanicalmachinery.helper;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.SimpleSound;
+import net.minecraft.util.SoundEvent;
+
+public class SoundHelper {
+    public static void playSound(SoundEvent sound) {
+        playSound(SimpleSound.master(sound, 1));
+    }
+
+    public static void playSound(ISound sound) {
+        Minecraft.getInstance().getSoundHandler().play(sound);
+    }
+}
