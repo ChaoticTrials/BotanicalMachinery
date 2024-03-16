26cbe1c69f65a5e2aaaa072bddb2164c68cd18c8
MelanX <git@melanx.de>
2024-03-16 22:12:23 +0100

    add tag for mechanical apothecary catalysts which allows to use more than seeds
    closes #61

--- src/main/java/de/melanx/botanicalmachinery/data/CommonTags.java
+++ src/main/java/de/melanx/botanicalmachinery/data/CommonTags.java
@@ -1,8 +1,13 @@
 package de.melanx.botanicalmachinery.data;
 
+import de.melanx.botanicalmachinery.BotanicalMachinery;
 import de.melanx.botanicalmachinery.ModBlocks;
+import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.moddingx.libx.annotation.data.Datagen;
 import org.moddingx.libx.datagen.provider.CommonTagsProviderBase;
@@ -10,6 +15,8 @@ import org.moddingx.libx.mod.ModX;
 
 @Datagen
 public class CommonTags extends CommonTagsProviderBase {
+
+    public static final TagKey<Item> MECHANICAL_APOTHECARY_CATALYSTS = TagKey.create(Registry.ITEM_REGISTRY, BotanicalMachinery.getInstance().resource("mechanical_apothecary_catalysts"));
     
     public CommonTags(ModX mod, DataGenerator generator, ExistingFileHelper fileHelper) {
         super(mod, generator, fileHelper);
@@ -17,6 +24,8 @@ public class CommonTags extends CommonTagsProviderBase {
 
     @Override
     public void setup() {
+        this.item(MECHANICAL_APOTHECARY_CATALYSTS).addTag(Tags.Items.SEEDS);
+
         this.block(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.manaEmeraldBlock);
         this.block(BlockTags.NEEDS_IRON_TOOL).add(ModBlocks.manaEmeraldBlock);
     }
